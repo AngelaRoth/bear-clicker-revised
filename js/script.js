0ede1c46e03bf24431d900d0887e638ad0975da9
Angela <Angela>
2017-03-14 20:11:57 -0400

    First Working Model (inferior to original class structure, I think!)

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-var allBears = document.getElementById('allBears');
-var bearList = document.getElementById('bearList');
-
-// The Bear Class. Takes as imput the name of the bear
-// and its position in the bearArray
 var Bear = function(name, i) {
   // Names of bears in nameArray correspond to names of jpg files.
   this.img = 'images/' + name + '.jpg';
   this.name = name;
   this.clicks = 0;
-
-  // Make the bear's entry in the Choose a Bear list
-  this.listItem = document.createElement('li');
-  this.listItem.innerHTML = this.name;
-  // Attach an event listener which calls the bearChosen function on
-  // the bear stored in this bear's place in the bearArray
-  this.listItem.addEventListener('click', function() {
-    bearChosen(i);
-  });
-
 
   // Make the Heading for the Bear Container
   this.nameElem = document.createElement('h2');
@@ -31,7 +16,7 @@ var Bear = function(name, i) {
   // Attach an event listener which calls the bearClicked function on
   // the bear stored in this bear's place in the bearArray
   this.imgElem.addEventListener('click', function() {
-    bearClicked(i);
+    octopus.bearClicked();
   });
 
   // Make the Paragraph which contains info on number of clicks (salmon)
@@ -43,69 +28,105 @@ var Bear = function(name, i) {
   // Make the container which will hold all this bear's information
   this.container = document.createElement('div');
   this.container.className = 'bearContainer';
-
-};
-
-// Add bear as a child of #bearList element
-Bear.prototype.enterList = function() {
-  bearList.appendChild(this.listItem);
-}
-
-// Append all of a bear's information to its container and
-// add container as a child of #allBears element
-Bear.prototype.enterHTML = function() {
   this.container.appendChild(this.nameElem);
   this.container.appendChild(this.imgElem);
   this.container.appendChild(this.countPara);
-  allBears.appendChild(this.container);
 };
 
-// Remove the bear's container from the children of #allBears element
-Bear.prototype.leaveHTML = function() {
-  allBears.removeChild(this.container);
-}
+var model = {
+  // All our bears: names correspond to file names of jpg's
+  nameArray: ['vonGrizzmon', 'Fuzzy', 'Whitey', 'Vera', 'Zeke', 'Moz', 'Splash', 'Colleen'],
+  /*numBears: model.nameArray.length,*/
+  bearArray: [],
+  currentBear: null,
 
-// Called when a bear's name is chosen for display.
-// If the bear is not already displayed, it removes currently displayed
-// bear and adds newly chosen bear; otherwise it augments the bear's salmon.
-// Runs on the bear located at position (i) of the bearArray.
-function bearChosen(i) {
-  if (bearArray[i] !== currentBear) {
-    currentBear.leaveHTML();
-    bearArray[i].enterHTML();
-    currentBear = bearArray[i];
-  } else {
-    bearClicked(i);
+  init: function() {
+    // Make a NEW Bear for every name in nameArray
+    // Store new Bears in bearArray
+    for (var i = 0; i < this.nameArray.length; i++) {
+      this.bearArray[i] = new Bear(this.nameArray[i], i);
+    }
+
+    this.currentBear = this.bearArray[0];
   }
-}
+};
 
-// Augment's bear's salmon when a their image is clicked.
-// Makes container background red when salmon number is a multiple of five.
-// Runs on the bear located at position (i) of the bearArray.
-function bearClicked(i) {
-  bearArray[i].clicks ++;
-  bearArray[i].countPara.innerHTML = bearArray[i].clicks + ' Salmon';
+var octopus = {
+  init: function() {
+    model.init();
+    model.currentBear = model.bearArray[0];
 
-  if (bearArray[i].clicks % 5 === 0) {
-    bearArray[i].container.classList.add('fiveClick');
-  } else {
-    bearArray[i].container.classList.remove('fiveClick');
+    listView.init();
+    bearView.init(model.currentBear);
+  },
+
+  getBears: function() {
+    return model.bearArray;
+  },
+
+  getCurrentBear: function() {
+    return model.currentBear;
+  },
+
+  setCurrentBear: function(bear) {
+    model.currentBear = bear;
+  },
+
+  bearClicked: function(i) {
+    model.currentBear.clicks ++;
+    model.currentBear.countPara.innerHTML = model.currentBear.clicks + ' Salmon';
+
+    if (model.currentBear.clicks % 5 === 0) {
+      model.currentBear.container.classList.add('fiveClick');
+    } else {
+      model.currentBear.container.classList.remove('fiveClick');
+    }
   }
-}
+};
 
-// All our bears: names correspond to file names of jpg's
-var nameArray = ['vonGrizzmon', 'Fuzzy', 'Whitey', 'Vera', 'Zeke', 'Moz', 'Splash', 'Colleen'];
-var numBears = nameArray.length;
-var bearArray = [];
+var listView = {
+  init: function() {
+    this.bearList = document.getElementById('bearList');
+    this.render();
+  },
 
-// Make a NEW Bear for every name in nameArray
-// Store new Bears in bearArray
-for (var i = 0; i < numBears; i++) {
-  bearArray[i] = new Bear(nameArray[i], i);
-  bearArray[i].enterList();
-}
+  render: function() {
+    var bear, listItem;
+    var bearArray = octopus.getBears();
+    var currentBear = octopus.getCurrentBear();
+    for (var i = 0; i < bearArray.length; i++) {
+      bear = bearArray[i];
+      listItem = document.createElement('li');
+      listItem.innerHTML = bear.name;
+      listItem.addEventListener('click', (function(bearCopy) {
+        return function() {
+          /*if (bearCopy !== currentBear) {*/
+            bearView.removeCurrentBear();
+            bearView.render(bearCopy);
+            octopus.setCurrentBear(bearCopy);
+          /*}*/
+        }
+      })(bear));
 
-// When site is first loaded, the first bear in the array
-// is the "currently displayed bear"
-bearArray[0].enterHTML();
-var currentBear = bearArray[0];
+      this.bearList.appendChild(listItem);
+    }
+  }
+};
+
+var bearView = {
+  init: function(firstBear) {
+    this.allBears = document.getElementById('allBears');
+    this.render(firstBear);
+  },
+
+  removeCurrentBear: function() {
+    var currentBear = octopus.getCurrentBear();
+    this.allBears.removeChild(currentBear.container);
+  },
+
+  render: function(bear) {
+    this.allBears.appendChild(bear.container);
+  }
+};
+
+octopus.init();
